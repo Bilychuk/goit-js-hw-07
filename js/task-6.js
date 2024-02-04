bf3262d4c45a18ac31a577a9d7a408af07db2640
Bilychuk <bilychukkateryna@ukr.net>
2024-02-04 23:03:45 +0200

    corrections

--- js/task-6.js
+++ js/task-6.js
@@ -5,33 +5,27 @@ const boxesCollection = document.querySelector("#boxes");
 const btnCreate = document.querySelector("[data-create]");
 const btnDestroy = document.querySelector("[data-destroy");
 
-input.addEventListener("input", createBoxes);
-btnCreate.addEventListener("click", onClick);
+btnCreate.addEventListener("click", () => createBoxes(input.value.trim()));
 btnDestroy.addEventListener("click", destroyBoxes);
 
-function onClick() {
-  const amount = input.value.trim();
+function createBoxes(amount) {
+  destroyBoxes();
   if (amount >= 1 && amount <= 100) {
-    createBoxes(amount);
+    const boxes = [];
+    for (let i = 0; i < amount; i += 1) {
+      const box = document.createElement("div");
+      const size = 30 + i * 10;
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxes.push(box);
+      input.value = "";
+    }
+    boxesCollection.append(...boxes);
   } else {
     input.value = "";
     return alert(`Number should be from 1 to 100`); 
   }
-}
-
-function createBoxes(amount) {
-  destroyBoxes();
-  const boxes = [];
-  for (let i = 0; i < amount; i += 1) {
-    const box = document.createElement("div");
-    const size = 30 + i * 10;
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxes.push(box);
-    input.value = "";
-  }
-  boxesCollection.append(...boxes);
 }
 
 function destroyBoxes() {
